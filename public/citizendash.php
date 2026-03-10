d0abbc70b0ff17aa7f0dab7f2773485568caab94
EXEQUIELKENT <130205735+EXEQUIELKENT@users.noreply.github.com>
2026-03-10 18:24:00 +0800

    Update citizendash.php

--- public/citizendash.php
+++ public/citizendash.php
@@ -1,11 +1,3 @@
-<?php
-// For local development and domain (show correct path for logo)
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-    $BASE_URL = '/public/';
-} else {
-    $BASE_URL = '/public/';
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
