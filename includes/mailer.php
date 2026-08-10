0959ad62b511325ace546a698ae8263d4c2f862b
EXEQUIELKENT <130205735+EXEQUIELKENT@users.noreply.github.com>
2026-08-10 14:05:55 +0800

    Update mailer.php

--- includes/mailer.php
+++ includes/mailer.php
@@ -18,7 +18,7 @@ function mainLguMailer(): PHPMailer
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'lguportal2026@gmail.com';
-    $mail->Password = 'iispigiketmacsma';
+    $mail->Password = 'whcq ieci jieq dctj';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
     $mail->CharSet = 'UTF-8';
